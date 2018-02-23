cf409c4c344e0a1456ed2c43435cb7f87b649dde
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2018-02-23 20:02:27 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+"""
+:copyright: (c) 2014 by Taehoon Kim.
+:license: BSD, see LICENSE for more details.
+このライブラリはTaehoon Kim氏が開発し、Aidenが改良したライブラリです。
+"""
+
+from distutils.core import setup
+
+setup(
+    name         = "linepy",
+    version      = "3.8.0",
+    author       = "Aiden",
+    author_email = "rootmelo92118@gmail.com",
+    package_dir  = {"": "linepy"},
+    install_requires = [
+    "linepy",
+    ]
+)
